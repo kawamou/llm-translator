73f619d1f9c0b32c56476e0bed207399ecf9166d
kawamou <kwmkzsd@gmail.com>
2024-06-09 23:30:53 +0900

    upload.tsの更新

--- app/routes/upload.ts
+++ app/routes/upload.ts
@@ -3,33 +3,60 @@ import type { ActionFunction } from "@remix-run/node";
 import { execFile } from "node:child_process";
 import { promisify } from "node:util";
 import { unlink, writeFile } from "node:fs/promises";
-import { pipeline, Readable } from "node:stream";
-import { tmpdir } from "node:os";
 import path from "node:path";
+import ffmpeg from "fluent-ffmpeg";
+
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const execFileAsync = promisify(execFile);
 
+// Whisper.cpp が 16kHz の wav ファイルのみを受け付けるため変換する
+async function convertToWav(input: string, output: string) {
+	return new Promise<void>((resolve, reject) => {
+		ffmpeg(input)
+			.outputOptions(["-ar 16000", "-ac 1"])
+			.toFormat("wav")
+			.on("end", () => {
+				resolve();
+			})
+			.on("error", (error) => {
+				reject(error);
+			})
+			.save(output);
+	});
+}
+
 export const action: ActionFunction = async ({ request }) => {
-	const buffer = await request.arrayBuffer();
-	const tempFilePath = path.join(tmpdir(), `${Date.now()}.webm`);
-	await writeFile(tempFilePath, Buffer.from(buffer));
-
-	try {
-		// const { stdout } = await execFileAsync("./whisper", [
-		// 	"-m",
-		// 	"models/ggml-base.bin",
-		// 	"-f",
-		// 	tempFilePath,
-		// ]);
-		// const transcription = stdout.trim();
-
-		// 一時ファイルを削除
-		await unlink(tempFilePath);
-
-		// return json({ transcription });
-		return json({ hoge: "hoge" });
-	} catch (error) {
-		console.error(`Error processing audio file: ${error}`);
-		return json({ error: "Failed to process audio file" }, { status: 500 });
+	if (!request.body) {
+		return { status: 400, body: "No file uploaded" };
 	}
+
+	const buffer = await request.arrayBuffer();
+	const tempWebmFilePath = path.join(".", `${Date.now()}.webm`);
+	const tempWavFilePath = path.join(".", `${Date.now()}.wav`);
+	await writeFile(tempWebmFilePath, Buffer.from(buffer));
+
+	await convertToWav(tempWebmFilePath, tempWavFilePath);
+
+	const { stdout, stderr } = await execFileAsync(
+		path.resolve(__dirname, "../../stt/whisper.cpp/main"),
+		[
+			"-m",
+			path.resolve(__dirname, "../../stt/whisper.cpp/models/ggml-base.bin"),
+			"-f",
+			tempWavFilePath,
+			"-nt",
+			"-l",
+			"ja",
+		],
+	);
+
+	console.log("transcription: ", stdout, stderr);
+
+	await unlink(tempWebmFilePath);
+	await unlink(tempWavFilePath);
+
+	return json({ stdout });
 };
